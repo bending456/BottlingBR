27f8056a070b11fa432df7dee3a3b7ebf57c1283
bending456 <bending456@gmail.com>
2023-06-05 09:33:59 -0400

    Update MBRDrafter.py

--- MBRDrafter.py
+++ MBRDrafter.py
@@ -53,7 +53,7 @@ run.bold = True
 run.font.size = Pt(10)
 run.font.color.rgb = RGBColor(255, 0, 0)
 paragraph.paragraph_format.space_before=Pt(10)
-paragraph.paragraph_format.space_after=Pt(10)
+#paragraph.paragraph_format.space_after=Pt(10)
 
 ###------ Header of Document - Name
 authorname = st.sidebar.text_input("Write the name of author in here ...")
