6779acb6ee0811576dd19432f6221a6e56e7340d
bending456 <bending456@gmail.com>
2023-06-16 11:14:25 -0400

    Update MBRDrafter.py

--- MBRDrafter.py
+++ MBRDrafter.py
@@ -423,21 +423,21 @@ if primary:
    table = document.tables[9]
    cell = table.cell(6,1)
    paragraph = cell.add_paragraph()
-   text = '\nRecord the batch number and quantity of '+productName+' available in the spaces provided.\n'
+   text = 'Record the batch number and quantity of '+productName+' available in the spaces provided.\n'
    run = paragraph.add_run(text)
-   run.font.size = Pt(12)
+   run.font.size = Pt(11)
 
    table = document.tables[10]
    for i in [1,3,5]:
       cell = table.cell(i,1)
       paragraph = cell.add_paragraph()
-      text1 = '\nCollect one hundred (100) '+productName+' from the beginning of the bulk product allocated for this batch and printweigh (in grams) using the space provided. Record the scale number in the space provided.\n'
+      text1 = 'Collect one hundred (100) '+productName+' from the beginning of the bulk product allocated for this batch and printweigh (in grams) using the space provided. Record the scale number in the space provided.\n'
       text2 = '\nNote: All product used for the 100 ct. weights are to be returned to bulk product.\n'
       run1 = paragraph.add_run(text1)
       run2 = paragraph.add_run(text2)
       run2.bold = True
-      run1.font.size = Pt(12)
-      run2.font.size = Pt(12)
+      run1.font.size = Pt(11)
+      run2.font.size = Pt(11)
    
 
 #################################################################################
